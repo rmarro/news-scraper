e9ff76a94c84c114399b4925dbd512697b106fa3
rmarro <30510953+rmarro@users.noreply.github.com>
2018-03-31 18:08:56 -0500

    can scrape titles and links from blog

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        // Trying to avoid pushing articles that already exist when scraping. But what will happen if it tries to add a bunch and some already exist? will only the missing ones get added, or will the whole thing result in an error??
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    saved: {
+        type: Boolean,
+    },
+    // Figure out how to be able to push multiple notes to the array
+    notes: []
+    // This is to just associate one with the article:
+    // note: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Note"
+    //   }
+
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -2,18 +2,56 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
 
 // Initialize Express
 var app = express();
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
+
+app.get("/scrape", function (req, res) {
+    request("https://blog.github.com/", function (error, response, html) {
+
+        var $ = cheerio.load(html);
+        var results = [];
+
+
+        $("div.mb-5").each(function (i, element) {
+
+            // I think the articleModel needs to get used here?
+            // create a new articleModel, and then push that to the results, then insert the results
+            // var article = new Article
+
+            var title = $(element).children("h1.lh-condensed").text();
+            var link = $(element).children("h1.lh-condensed").children().attr("href");
+
+            results.push({
+                title, link
+            });
+
+            // db.scrapeData.insert(results);
+        });
+        res.json(results);
+    });
+});
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`App running on ${port}`);
 });
